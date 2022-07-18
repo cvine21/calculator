3d1bd383c5b21808a750744c9a06585cbeb3f522
Claudine Vine <cvine@ev-s3.kzn.21-school.ru>
2022-07-18 22:05:27 +0300

    feat: day03 added

---
+++ day03/chapter_1/myScript.js
@@ -0,0 +1,95 @@
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear-button');
+const previousOperandTextELement = document.querySelector('.previous-operand');
+const currentOperandTextELement = document.querySelector('.current-operand');
+
+
+class Calculator {
+
+	constructor(previousOperandTextELement, currentOperandTextELement) {
+		this.previousOperandTextELement = previousOperandTextELement;
+		this.currentOperandTextELement = currentOperandTextELement;
+		this.clear();
+	}
+
+	clear() {
+		this.currentOperand = '';
+		this.previousOperand = '';
+		this.operation = undefined;
+	}
+
+	appendNumber(number) {
+		if (number === '.' && this.currentOperand.includes('.')) return ;
+		this.currentOperand = this.currentOperand.toString() + number.toString();
+	} 
+
+	chooseOperation(operation) {
+		if (this.currentOperand === '') return ;
+		if (this.previousOperand !== '') {
+			this.compute();
+		}
+		this.operation = operation;
+		this.previousOperand = this.currentOperand;
+		this.currentOperand = '';
+	}
+
+	compute() {
+		let	computation;
+		const prev = parseFloat(this.previousOperand);
+		const current = parseFloat(this.currentOperand);
+
+		if (isNaN(prev) || isNaN(current)) return ;
+		switch (this.operation) {
+			case '+':
+				computation = prev + current;
+				break ;
+			case '-':
+				computation = prev - current;
+				break ;
+			case '*':
+				computation = prev * current;
+				break ;
+			case '/':
+				computation = prev / current;
+				break ;
+			default:
+				return ;
+		}
+		this.currentOperand = computation;
+		this.operation = undefined;
+		this.previousOperand = '';
+	}
+
+	updateDisplay() {
+		this.previousOperandTextELement.innerText = this.previousOperand;
+		this.currentOperandTextELement.innerText = this.currentOperand;
+	}
+}
+
+let calculator = new Calculator(previousOperandTextELement, currentOperandTextELement);
+
+numberButtons.forEach(button => {
+	button.addEventListener('click', () =>  {
+		calculator.appendNumber(button.innerText);
+		calculator.updateDisplay();
+	})
+})
+
+operationButtons.forEach(button => {
+	button.addEventListener('click', () =>  {
+		calculator.chooseOperation (button.innerText);
+		calculator.updateDisplay();
+	})
+})
+
+equalsButton.addEventListener('click', button => {
+	calculator.compute();
+	calculator.updateDisplay();
+})
+
+clearButton.addEventListener('click', button => {
+	calculator.clear();
+	calculator.updateDisplay();
+}) 
